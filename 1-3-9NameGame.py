58c385cc542240d0afaa6c7aea83b7a540d056cc
alimovAN19 <47255589+alimovAN19@users.noreply.github.com>
2019-02-05 13:26:53 -0500

    Add files via upload

--- 1-3-9NameGame.py
+++ 1-3-9NameGame.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 '''Alter the code to produce a different output if your name is entered.  As always, start a log file
@@ -7,8 +8,17 @@ the classroom alonf with your code'''
 def who_is_this():
     name=raw_input('Please enter the name of someone in this class:')
     
-    if name=='Carlos':
-        print(name,' favorite phrase is : "Now the fun part is..."')
+    if name=='Abbos':
+        print(name,""" favorite phrase is : "First according to the height of a Minion (which is 3.5 feet on average)"
+        "Gru is 4 minions tall, which means he is a godly size of 14 feet tall. Furthermore if any of you remember"
+        "the original Despicable Me,"
+        "you know there is a scene when Vector kidnaps the three girls and shoots a series of heat-seeking misses"
+        "at Gru, he then dodges them all. According to the speed of an average ballistic missile (1900 mph) and "
+        "the size of the missile according to his ankle size, "
+        "Gru can perceive and move at such a speed that the missiles only move 9.5 miles per hour, 0.5% of their" 
+        "original speed. After this Gru punches a shark and it is paralyzed meaning its spine is probably shattered,"
+        "to remind you it would require a force greater than 3,000 newtons to fracture the spine."
+        "That’s equal to the impact created by a 500-pound car crashing into a wall at 30 miles per hour. I rest my case.""")
     
     return
     
